ba6e84e802c45754c3edecd25de1824a1457f35b
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-17 19:59:08 +0500

    .

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-require_once 'connect.php';
+//require_once 'connect.php';
 include 'Telegram.php';
  $telegram = new Telegram('5601653365:AAGjIarcmGayfd54MBSvVf1Qznc2BoQlWPY');
 $chat_id=$telegram->ChatID();
 $text=$telegram->Text();
 $telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
+echo "alhamdulillah";
