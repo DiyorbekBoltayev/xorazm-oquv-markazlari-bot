37b73e051133053db002f7859b24928bc3ca9b9a
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-17 19:56:24 +0500

    yaratildi

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b920520e2d5cfd";
+$password = "53cac20c";
+$db="heroku_6ecbfd4e77df4ab";
+$conn = mysqli_connect("$servername", "$username", "$password","$db");
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'connect.php';
+include 'Telegram.php';
+ $telegram = new Telegram('5601653365:AAGjIarcmGayfd54MBSvVf1Qznc2BoQlWPY');
+$chat_id=$telegram->ChatID();
+$text=$telegram->Text();
+$telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
