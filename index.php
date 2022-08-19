798abf91faab351c8ac257537a235935d9e8027a
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-08-19 18:09:18 +0500

    database connection

--- index.php
+++ index.php
@@ -4,5 +4,25 @@ include 'Telegram.php';
  $telegram = new Telegram('5601653365:AAGjIarcmGayfd54MBSvVf1Qznc2BoQlWPY');
 $chat_id=$telegram->ChatID();
 $text=$telegram->Text();
-$telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
-echo "alhamdulillah";
+
+if($text=="/start"){
+    chooseLanguage();
+}else{
+    $telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
+}
+
+function chooseLanguage(){
+    global $telegram,$chat_id;
+    $options=[
+        [$telegram->buildKeyboardButton("🇺🇿 O'zbek tili")],
+        [$telegram->buildKeyboardButton("🇷🇺 Русский язык")]
+    ];
+    $keyboard=$telegram->buildKeyBoard($options,true,true);
+    $content=[
+        'chat_id'=>$chat_id,
+        'reply_markup'=>$keyboard,
+        'text'=>"Iltimos tilni tanlang: \nВыберите язык:"
+    ];
+    $telegram->sendMessage($content);
+}
+
