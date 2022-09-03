797a9c66e3dc9019049cb0989a1e1c63c1abaa23
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-09-03 18:44:03 +0500

    2-bot test

--- obhavo.php
+++ obhavo.php
@@ -7,3 +7,70 @@ $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
 echo "ob-havo";
+
+if($text=='/start'){
+    showStart();
+}
+elseif ($text=='⌨️ Shahar nomini kiritish'){
+    askCity();
+}
+
+function showStart(){
+    global  $telegram,$chat_id;
+    $text="Assalomu alaykum. Ob-havoni bilish uchun quyidagi tugmalardan birini tanlang 👇";
+    $options=[
+
+      [$telegram->buildKeyboardButton('🔻 Joylashuvdagi ob-havo',false,true)],
+        [$telegram->buildKeyboardButton('⌨️ Shahar nomini kiritish')]
+    ];
+    $keyb=$telegram->buildKeyBoard($options,false,true);
+    $content=[
+        'chat_id'=>$chat_id,
+        'reply_markup'=>$keyb,
+        'text'=>$text
+    ];
+    $telegram->sendMessage($content);
+}
+
+function askCity(){
+    global $telegram,$chat_id;
+    file_put_contents('obhavo.txt','ask');
+    $content=[
+        'chat_id'=>$chat_id,
+        'text'=>'🔆 Ob-havoni bilish uchun Shahar nomini yozib yuboring.'
+    ];
+    $telegram->sendMessage($content);
+}
+function findWeather(){
+    global $telegram,$chat_id,$text;
+    file_put_contents('obhavo.txt','topildi');
+
+    $request="https://api.openweathermap.org/data/2.5/weather?q=".$text."&appid=253468cdc02c66c0ccb7393c8d3ce4e7";
+    $data=json_decode(file_get_contents($request),true);
+    if($data['cod']==200){
+        $text=matnTayyor($data);
+        $content=[
+            'chat_id'=>$chat_id,
+            'text'=>$text
+        ];
+        $telegram->sendMessage($content);
+
+
+
+    }else{
+        $content=[
+            'chat_id'=>$chat_id,
+            'text'=>"😢 Bunday shahar topilmadi, Iltimos qayta urunib ko'ring"
+        ];
+        $telegram->sendMessage($content);
+        askCity();
+    }
+
+}
+function matnTayyor($data):string{
+    $javob="💭 Ob-havo ma'lumoti:\n";
+    $javob.="🌆 Shahar: ".$data['name']." \n";
+    $javob.="🌡 Harorat: ".($data['main']["temp"]-273)." \n";
+    $javob.="Shamol tezligi: ".$data['wind']["speed"]." m/s \n";
+    return $javob;
+}
