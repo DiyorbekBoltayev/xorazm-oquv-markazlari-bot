ae93b16ad09574eb8f1e92b4cc473c57fffe6632
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-09-03 16:20:27 +0500

    2-bot test

--- obhavo.php
+++ obhavo.php
@@ -6,4 +6,4 @@ $telegram = new Telegram('5520195616:AAGp7GKgJOsaJa2rI-u_Tj-UfwOA8DB_qy0');
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
-echo "bismillah";
+echo "ob-havo";
