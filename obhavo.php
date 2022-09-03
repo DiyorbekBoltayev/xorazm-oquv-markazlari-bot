0348cb7a151d55c768ce4349eb3f9e3f34838d0a
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-09-03 22:02:05 +0500

    2-bot test

--- obhavo.php
+++ obhavo.php
@@ -6,6 +6,7 @@ $telegram = new Telegram('5520195616:AAGp7GKgJOsaJa2rI-u_Tj-UfwOA8DB_qy0');
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 $message=$telegram->getData();
+$message=$message['message'];
 if($text=='/start'){
     showStart();
 }
