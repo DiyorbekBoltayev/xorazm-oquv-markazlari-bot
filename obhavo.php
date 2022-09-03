ade59c250f27c3d56e670f862a5fc1e4e6b8a15e
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-09-03 18:46:42 +0500

    2-bot test

--- obhavo.php
+++ obhavo.php
@@ -5,14 +5,15 @@ include 'Telegram.php';
 $telegram = new Telegram('5520195616:AAGp7GKgJOsaJa2rI-u_Tj-UfwOA8DB_qy0');
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
-$telegram->sendMessage(['chat_id'=>$chat_id,'text'=>$text]);
-echo "ob-havo";
 
 if($text=='/start'){
     showStart();
 }
 elseif ($text=='⌨️ Shahar nomini kiritish'){
     askCity();
+}
+elseif (file_get_contents('obhavo.txt')=='ask'){
+    findWeather();
 }
 
 function showStart(){
